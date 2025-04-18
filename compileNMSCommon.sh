a41e73ad1f372327a6ebf017e9a7f08f673f8465
DerEchtePilz <81232921+DerEchtePilz@users.noreply.github.com>
2025-04-18 17:21:10 +0200

    Make compileNMSCommon.sh more compact

--- compileNMSCommon.sh
+++ compileNMSCommon.sh
@@ -1,23 +1,13 @@
-echo "Compiling NMS_Common for Spigot 1.20 and 1.20.1..."
-mvn clean package -Dmaven.source.skip=true -Dmaven.javadoc.skip=true -pl :commandapi-bukkit-nms-common -am -P Platform.Bukkit,Spigot_1_20_R1 --quiet
+compileVersion() {
+	echo "Compiling NMS_Common for Spigot $1..."
+	mvn clean package -Dmaven.source.skip=true -Dmaven.javadoc.skip=true -pl :commandapi-bukkit-nms-common -am -P Platform.Bukkit,"$2" --quiet
+}
 
-echo "Compiling NMS_Common for Spigot 1.20.2..."
-mvn clean package -Dmaven.source.skip=true -Dmaven.javadoc.skip=true -pl :commandapi-bukkit-nms-common -am -P Platform.Bukkit,Spigot_1_20_R2 --quiet
-
-echo "Compiling NMS_Common for Spigot 1.20.3 and 1.20.4..."
-mvn clean package -Dmaven.source.skip=true -Dmaven.javadoc.skip=true -pl :commandapi-bukkit-nms-common -am -P Platform.Bukkit,Spigot_1_20_R3 --quiet
-
-echo "Compiling NMS_Common for Spigot 1.20.5 and 1.20.6..."
-mvn clean package -Dmaven.source.skip=true -Dmaven.javadoc.skip=true -pl :commandapi-bukkit-nms-common -am -P Platform.Bukkit,Spigot_1_20_R4 --quiet
-
-echo "Compiling NMS_Common for Spigot 1.21 and 1.21.1..."
-mvn clean package -Dmaven.source.skip=true -Dmaven.javadoc.skip=true -pl :commandapi-bukkit-nms-common -am -P Platform.Bukkit,Spigot_1_21_R1 --quiet
-
-echo "Compiling NMS_Common for Spigot 1.21.2 and 1.21.3..."
-mvn clean package -Dmaven.source.skip=true -Dmaven.javadoc.skip=true -pl :commandapi-bukkit-nms-common -am -P Platform.Bukkit,Spigot_1_21_R2 --quiet
-
-echo "Compiling NMS_Common for Spigot 1.21.4..."
-mvn clean package -Dmaven.source.skip=true -Dmaven.javadoc.skip=true -pl :commandapi-bukkit-nms-common -am -P Platform.Bukkit,Spigot_1_21_R3 --quiet
-
-echo "Compiling NMS_Common for Spigot 1.21.5..."
-mvn clean package -Dmaven.source.skip=true -Dmaven.javadoc.skip=true -pl :commandapi-bukkit-nms-common -am -P Platform.Bukkit,Spigot_1_21_R4 --quiet
+compileVersion "1.20 and 1.20.1" "Spigot_1_20_R1"
+compileVersion "1.20.2" "Spigot_1_20_R2"
+compileVersion "1.20.3 and 1.20.4" "Spigot_1_20_R3"
+compileVersion "1.20.5 and 1.20.6" "Spigot_1_20_R4"
+compileVersion "1.21 and 1.21.1" "Spigot_1_21_R1"
+compileVersion "1.21.2 and 1.21.3" "Spigot_1_21_R2"
+compileVersion "1.21.4" "Spigot_1_21_R3"
+compileVersion "1.21.5" "Spigot_1_21_R4"
